94e6891e726e5442d178faa3b0a4d8deed0e7c63
Hunter Woody <hunterwoody@gmail.com>
2017-06-07 15:46:30 -0400

    adding files

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	let text;
+	let author;
+	function getNewQuote() {
+		$.ajax({
+			url: 'http://api.forismatic.com/api/1.0/',
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp'
+			},
+			success: function(data) {
+				text = data.quoteText;
+				author = data.quoteAuthor;
+				$('.quote').html('"' + text + '"');
+				if (author) {
+					$('.author').html('- ' + author);
+				}
+				else {
+					$('.author').html('- Unknown');
+				}
+			}
+		})
+	}
+	getNewQuote();
+
+	$('#tweetQuote').on('click', function() {
+		window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + text + '"' + ' - ' + author));
+	});
+
+	$('#newQuote').on('click', function() {
+		getNewQuote();
+	});
+});
